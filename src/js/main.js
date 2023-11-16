ed2be1cc73da2cf16ef75a16d869affb098d7ceb
Egor <koropeczegor@gmail.com>
2023-11-16 20:06:49 +0300

    slider fix

--- src/js/main.js
+++ src/js/main.js
@@ -42,20 +42,22 @@ new Swiper(".social__slider", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  // pagination: {
-  //   el: "swiper-pagination",
-  //   clickable: true,
-  //   dinamicBullets: true,
-  // },
-  // //переключения спомощью стрелок на квиатуры
-  // keyboard: {
-  //   enabled: true,
-  //   onlyInViewport: true,
-  //   pageUpDown: true,
-  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dinamicBullets: true,
+  },
+  //переключения с помощью стрелок на клавиатуре
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  //управления с помощью мыши
   // mousewheel: {
   //   sensitivity: 1,
   //   // eventsTarget: ".slider__img",
   // },
-  // slidesPerView: 1,
+  slidesPerView: 5,
+  spaceBetween: 30,
 });
